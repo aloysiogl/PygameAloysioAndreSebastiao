b578f4bc584e50d48e7174578cfff28a69b97757
aloysio <aloysiogl@gmail.com>
2018-03-11 20:52:52 -0300

    Created representations class and a representation for color.

---
+++ frame/core/Representations.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class Color:
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+    yellow = (247, 251, 0)

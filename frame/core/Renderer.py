9962b66ec5be413fef307e6a9fddcfbc59f6096c
Unknown <aloysiogl@gmail.com>
2018-03-12 10:35:55 -0300

    Created renderer class

---
+++ frame/core/Renderer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+
+class Renderer:
+    __game_display = 0
+
+    @classmethod
+    def set_display(cls, display):
+        """
+        This method sets the display for rendering
+        :param display:
+        """
+        cls.__game_display = display
+
+    @classmethod
+    def render_background(cls, bg_material):
+        """
+        This method draws the background
+        :param bg_material:
+        """
+        cls.__game_display.fill(bg_material.color)

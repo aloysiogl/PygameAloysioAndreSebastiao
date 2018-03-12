867ada340eafcc2c1dd67399812543f5cd1940f9
Unknown <aloysiogl@gmail.com>
2018-03-12 10:33:18 -0300

    Created material class

---
+++ frame/components/Material.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+class Material:
+
+    def __init__(self, color):
+        """
+        A material has color as basic propriety
+        :param color:
+        """
+        self.color = color

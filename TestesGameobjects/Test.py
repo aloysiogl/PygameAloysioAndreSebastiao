08cb34393d6383ace13638f6b5ba1d8d0050dd28
Unknown <moreno6230@gmail.com>
2018-03-25 21:37:14 -0300

    update code

--- TestesGameobjects/Test.py
+++ TestesGameobjects/Test.py
@@ -22,6 +22,7 @@ class Test(GameObject):
 
     def update(self):
         if EventHandler.key_up:
+            print(2)
             self.transform.position -= Timer.get_dt() * 500 * Vector2(0, 1)
             self.transform.scale += 0.005
             self.buddy.mesh.material.color = Color.green

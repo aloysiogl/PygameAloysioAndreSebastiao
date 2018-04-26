8e418ab880c5ce98103b1387fac52509344a1997
beethovenbrandaofilho <beethovenbrandaofilho@gmail.com>
2018-04-26 01:28:41 -0300

    removed power bar

--- structure/scenes/MainScene.py
+++ structure/scenes/MainScene.py
@@ -9,11 +9,9 @@ from structure.game_objects.main_objects.EnemyGenerator import EnemyGenerator
 
 from structure.game_objects.main_objects.BlackWrap import BlackWrap
 from structure.game_objects.main_objects.LifeBar import LifeBar
-from structure.game_objects.main_objects.PowerBar import PowerBar
 from structure.game_objects.main_objects.MainController import MainController
 from structure.game_objects.background_animator.ReverseStarCreator import ReverseStarCreator
 
 player = Player(Vector2(300, 300))
 
-main_scene = Scene([player, EnemyGenerator(), ReverseStarCreator(), LifeBar(Vector2(0, 0), player), MainController()
-                       , PowerBar(Vector2(340, 0), player), BlackWrap(Vector2(0, 0)), BlackWrap(Vector2(340, 0))], Material(Color.black))
+main_scene = Scene([player, EnemyGenerator(), ReverseStarCreator(), LifeBar(Vector2(0, 0), player), MainController() , BlackWrap(Vector2(0, 0))], Material(Color.black))
